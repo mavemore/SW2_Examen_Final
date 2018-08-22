c75a48dd60349fa0e6a887118cb30a411b2069a8
Luis Zuñiga <lufezuro@gmail.com>
2018-08-22 14:10:08 -0500

    prueba 1

--- test.py
+++ test.py
@@ -2,12 +2,17 @@
 from __future__ import unicode_literals
 
 import unittest
-import src.cotizador
+import src.cotizador as cot
 
 class Test(unittest.TestCase):
 	#Incluya una pequeña descripción de lo que se prueba.
 	def test_cotizador_ID(self):
 		self.assertEqual('','')
+
+	def test_cotizar_seguro_1(self):
+		valor = cot.cotizar_seguro("Guayaquil", 18, "mujer", "casado", "osteoporosis", 0)
+		self.assertEqual(valor,'El valor calculado de su cotización es de 50.00')		
+
 
 if __name__ == '__main__':
 	unittest.main()
