6960e75741c508adc90480e10a9ec1fa909e4745
Luis Zuñiga <lufezuro@gmail.com>
2018-08-22 14:32:05 -0500

    pruebas de casos especiales

--- test.py
+++ test.py
@@ -49,6 +49,38 @@ class Test(unittest.TestCase):
 	def test_cotizar_seguro_edad_fuera_rango(self):
 		valor = cot.cotizar_seguro("Guayaquil", 0, "mujer", "casado", "osteoporosis", 0)
 		self.assertEqual(valor,'La edad ingresada no es válida. Debe de encontrarse entre 18 y 75 años.')
+	
+	def test_cotizar_seguro_valor_adicional_mujeres_jovenes_y_casadas(self):
+		valor = cot.cotizar_seguro("Guayaquil", 18, "mujer", "casado", "", 0)
+		self.assertEqual(valor,'El valor calculado de su cotización es de 50.00')
+
+	def test_cotizar_seguro_valor_adicional_hombres_jovenes_y_casadas(self):
+		valor = cot.cotizar_seguro("Guayaquil", 18, "hombre", "casado", "", 0)
+		self.assertEqual(valor,'El valor calculado de su cotización es de 30.00')
+
+	def test_cotizar_seguro_valor_adicional_mujeres_jovenes_y_soltera(self):
+		valor = cot.cotizar_seguro("Guayaquil", 18, "mujer", "soltero", "", 0)
+		self.assertEqual(valor,'El valor calculado de su cotización es de 40.00')
+
+	def test_cotizar_seguro_valor_adicional_hombres_jovenes_y_soltero(self):
+		valor = cot.cotizar_seguro("Guayaquil", 18, "hombre", "soltero", "", 0)
+		self.assertEqual(valor,'El valor calculado de su cotización es de 70.00')
+
+	def test_cotizar_seguro_valor_adicional_mujer_con_osteoporosis(self):
+		valor = cot.cotizar_seguro("Guayaquil", 41, "mujer", "soltero", "osteoporosis", 0)
+		self.assertEqual(valor,'El valor calculado de su cotización es de 65.00')
+
+	def test_cotizar_seguro_valor_adicional_hombre_con_infarto(self):
+		valor = cot.cotizar_seguro("Guayaquil", 41, "hombre", "soltero", "infarto", 0)
+		self.assertEqual(valor,'El valor calculado de su cotización es de 80.00')
+
+	def test_cotizar_seguro_valor_adicional_hombre_mayor_con_diabetes(self):
+		valor = cot.cotizar_seguro("Guayaquil", 61, "hombre", "soltero", "diabetes", 0)
+		self.assertEqual(valor,'El valor calculado de su cotización es de 80.00')
+
+	def test_cotizar_seguro_valor_adicional_mujer_mayor_con_cancer(self):
+		valor = cot.cotizar_seguro("Guayaquil", 61, "mujer", "soltero", "cancer", 0)
+		self.assertEqual(valor,'El valor calculado de su cotización es de 90.00')
 
 
 if __name__ == '__main__':
