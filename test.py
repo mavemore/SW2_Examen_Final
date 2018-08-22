2eae2c244ea739199458f3f9f728b750a11ee053
MariaBelenGuaranda <mguarand@espol.edu.ec>
2018-08-22 14:33:45 -0500

    testes coverage for function 3

--- test.py
+++ test.py
@@ -126,6 +126,73 @@ class Test(unittest.TestCase):
 		valor_adicional = calcular_valores_adicionales(edad,sexo,estado_civil,especial)
 		self.assertEqual(valor_adicional,50)
 
+	def test_cotizar_seguro(self):
+		"""Calcula el valor total del sguro cotizado al asegurado, 
+	siendo de Guayaquil, con el perfil del asegurado de la prueba test_calcular_cuota_0 y  test_calcular_vals_adicionales_mujer_casada
+	"""
+		sexo = 'mujer'
+		edad = 18
+		estado_civil = 'casado'
+		especial = None
+		dependientes = 0
+		ciudad = 'Guayaquil'
+		resultado = cotizar_seguro(ciudad, edad, sexo, estado_civil, especial, dependientes)
+		self.assertEqual(resultado,"El valor calculado de su cotización es de 50.00")
+
+	def test_cotizar_seguro_alta_edad(self):
+		"""Calcula el valor total del sguro cotizado al asegurado, 
+	siendo de Guayaquil, si no tiene dependientes, es mujer, tiene 76 años, casada,sin nada especial
+	"""
+		sexo = 'mujer'
+		edad = 76
+		estado_civil = 'casado'
+		especial = None
+		dependientes = 0
+		ciudad = 'Guayaquil'
+		resultado = cotizar_seguro(ciudad, edad, sexo, estado_civil, especial, dependientes)
+		self.assertEqual(resultado,"La edad ingresada no es válida. Debe de encontrarse entre 18 y 75 años.")
+
+	def test_cotizar_seguro_no_ciudad(self):
+		"""Calcula el valor total del sguro cotizado al asegurado, 
+	siendo de Babahoyo, con el perfil del asegurado de la prueba test_calcular_cuota_0 y  test_calcular_vals_adicionales_mujer_casada
+	"""
+		sexo = 'mujer'
+		edad = 18
+		estado_civil = 'casado'
+		especial = None
+		dependientes = 0
+		ciudad = 'Babahoyo'
+		resultado = cotizar_seguro(ciudad, edad, sexo, estado_civil, especial, dependientes)
+		self.assertEqual(resultado,"Saludcita no opera en la ciudad ingresada.")
+
+
+	def test_cotizar_seguro_5_depends(self):
+		"""Calcula el valor total del sguro cotizado al asegurado, 
+	siendo de Guayaquil, con 5 dependientes y con el perfil de asegurado de test_calcular_vals_adicionales_mujer_casada
+	"""
+		sexo = 'mujer'
+		edad = 18
+		estado_civil = 'casado'
+		especial = None
+		dependientes = 5
+		ciudad = 'Guayaquil'
+		resultado = cotizar_seguro(ciudad, edad, sexo, estado_civil, especial, dependientes)
+		self.assertEqual(resultado,"Solo se puede realizar la cotización para hasta 4 dependientes en línea. \
+			Por favor acérquese a la agencia y presente una solicitud.")
+
+	def test_cotizar_seguro_muchos_depends(self):
+		"""Calcula el valor total del sguro cotizado al asegurado, 
+	siendo de Guayaquil, con un exceso de dependientes (12) y con el perfil de asegurado de test_calcular_vals_adicionales_mujer_casada
+	"""
+		sexo = 'mujer'
+		edad = 30
+		estado_civil = 'casado'
+		especial = None
+		dependientes = 12
+		ciudad = 'Guayaquil'
+		resultado = cotizar_seguro(ciudad, edad, sexo, estado_civil, especial, dependientes)
+		self.assertEqual(resultado,"No se puede realizar una cotización para el valor ingresado de dependientes.")
+
 
 if __name__ == '__main__':
 	unittest.main()
